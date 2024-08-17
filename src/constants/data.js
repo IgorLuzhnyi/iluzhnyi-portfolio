8e7870f5276636b7e89efaaa207c81a0981404b2
Acer <luzhnyi.igor@gmail.com>
2024-08-17 11:25:55 +0300

    added the new project

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,16 +1,24 @@
 export const projects = [
   {
+    title: "Delivery App",
+    ghLink: "https://github.com/IgorLuzhnyi/delivery-app",
+    website: "https://igorluzhnyi.github.io/delivery-app/",
+    description:
+      "A pet project imitating an app where you can order fast food.",
+    techs: "React, redux toolkit, MUI",
+  },
+  {
     title: "Scream's Website",
-    ghLink: "https://github.com/IgorLuzhnyi/react-scary-website/tree/main",
-    website: "https://igorluzhnyi.github.io/react-scary-website/#/",
+    ghLink: "https://github.com/IgorLuzhnyi/react-scary-website/",
+    website: "https://igorluzhnyi.github.io/react-scary-website/",
     description:
       "Simple training website built with React. Technologies used here: react-router-dom, React hooks.",
     techs: "React",
   },
   {
     title: "Authorisation Form",
-    ghLink: "https://github.com/IgorLuzhnyi/styled-components-form/tree/main",
-    website: "https://igorluzhnyi.github.io/styled-components-form/#/",
+    ghLink: "https://github.com/IgorLuzhnyi/styled-components-form/",
+    website: "https://igorluzhnyi.github.io/styled-components-form/",
     description:
       "A form which takes only one user to authorise. The data is saved to local storage after sign up and taken from there when signing in.",
     techs: "React, styled-components",
