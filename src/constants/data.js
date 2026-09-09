0ba59809c7d75f67da65fad0a8996e226b1e0403
IgorLuzhnyi <luzhnyi.igor@gmail.com>
2026-09-09 15:31:45 +0300

    removed dummy project

--- src/constants/data.js
+++ src/constants/data.js
@@ -15,17 +15,9 @@ export const projects = [
       "A form which takes only one user to authorise. The data is saved to local storage after sign up and taken from there when signing in.",
     techs: "React, styled-components",
   },
-  {
-    title: "Star Wars Twitter",
-    ghLink: "https://github.com/IgorLuzhnyi/redux-posts",
-    website: "https://igorluzhnyi.github.io/redux-posts/",
-    description:
-      "A funny page with Twitter-like posts from SW universe. I practiced my Redux skills here",
-    techs: "React, Redux",
-  },
 ];
 
-export const myIntro = `I learn Frontend Development and constantly improving my skills. I have a few simple pet-projects by now and a month of commercial experience. Currently, I am working on a Ticket Tracker app using react-hook-form and React Context`;
+export const myIntro = `I learn Frontend Development and constantly improve my skills. I have a few simple pet-projects by now and a month of commercial experience. Currently, I am working on a Ticket Tracker app using react-hook-form and React Context`;
 
 export const CVlink =
   "https://drive.google.com/file/d/1fz_HOtXU-V3-eIZ-YbnYEOnFbv2uMafP/view?usp=drive_link";
